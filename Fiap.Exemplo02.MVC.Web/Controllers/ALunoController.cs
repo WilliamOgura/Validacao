ade85696f7d02c4a6700a269af45e379f7597be1
VINICIUS PEREIRA SEVERO <RM76743@fiap.com.br>
2016-11-01 18:23:20 -0200

    First Commit

---
+++ Fiap.Exemplo02.MVC.Web/Controllers/ALunoController.cs
@@ -0,0 +1,64 @@
+﻿using Fiap.Exemplo02.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fiap.Exemplo02.MVC.Web.Models;
+using System.Data.Entity;
+
+namespace Fiap.Exemplo02.MVC.Web.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private PortalContext ctx = new PortalContext();
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Aluno aluno)
+        {
+            ctx.Aluno.Add(aluno);
+            ctx.SaveChanges();
+            ViewBag.msg = "Cadastrado com sucesso";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            var lista = ctx.Aluno.ToList();
+            return View(lista);
+        }
+
+        [HttpPost]
+        public ActionResult Excluir(int id)
+        {
+            Aluno a = ctx.Aluno.Find(id);
+            ctx.Aluno.Remove(a);
+            ctx.SaveChanges();
+            return RedirectToAction("Listar");
+        }
+
+        [HttpGet]
+        public ActionResult Alterar(int id)
+        {
+            Aluno a = ctx.Aluno.Find(id);
+            return View(a);
+        }
+
+        [HttpPost]
+        public ActionResult Alterar(Aluno a)
+        {
+            ctx.Entry(a).State = EntityState.Modified;
+            ctx.SaveChanges();
+            ViewBag.msg = "Alterado com sucesso";
+            return View(a);
+        }
+
+    }
+}
