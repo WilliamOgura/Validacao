ff7ce9e2ad3b958c5a90ac5d42c47968d6f41c3b
Aluno Shift <alunoshift@fiap.com.br>
2016-11-04 15:17:51 -0200

    Adicionbado efeito toogle no input text desconto

---
+++ Fiap.Exemplo02.MVC.Web/scripts/principal.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $("#Bolsa").click(function () {
+        $("div.desconto").toggle("slow", function () {
+            if ($("div.desconto").css("display") == "none") {
+                $("#Desconto").val("");
+            }
+        });
+    });
+
+    //na hora de carregar o form
+    if($("#Bolsa").attr("checked")=="checked") {
+        $("div.desconto").css("display", "block");
+    }
+
+});
