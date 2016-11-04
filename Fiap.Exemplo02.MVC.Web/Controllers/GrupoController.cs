b9d4a64b239d8323948a4a77825e22eb13763bc5
Aluno Shift <alunoshift@fiap.com.br>
2016-11-04 18:33:03 -0200

    Adicionado entidades grupo e projeto

---
+++ Fiap.Exemplo02.MVC.Web/Controllers/GrupoController.cs
@@ -0,0 +1,39 @@
+﻿using Fiap.Exemplo02.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo02.MVC.Web.Controllers
+{
+    public class GrupoController : Controller
+    {
+        private PortalContext ctx = new PortalContext();
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Grupo grupo)
+        {
+            ctx.Grupo.Add(grupo);
+            //ctx.Projeto.Add(projeto);
+            ctx.SaveChanges();
+            TempData["msg"] = "Cadastro efetuado com sucesso";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            List<Grupo> grupos = ctx.Grupo.ToList();
+            return View(grupos);
+        }
+    }
+
+   
+}
